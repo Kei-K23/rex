380d1eb125558e9e93230651a525e8976ba67a11
Kei-K23 <keiksl2301@gmail.com>
2024-08-29 11:24:18 +0630

    Add lexer class to generate tokens from string

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 #include <string>
 #include <vector>
 #include <memory>
+#include <cctype>
 
-// Define token type for lexer
+/*
+The TokenType enumeration defines the types of tokens the lexer can generate.
+*/
 enum class TokenType
 {
     LITERAL,
@@ -12,7 +15,9 @@ enum class TokenType
     END
 };
 
-// Structure for token with constructor
+/*
+The Token struct encapsulates a token's type and value.
+*/
 struct Token
 {
     TokenType type; // Type of token
@@ -20,4 +25,47 @@ struct Token
 
     // (Constructor) initializes a Token with a specified type and an optional value. The default value for value is '\0', which is useful for tokens that do not need a specific character value (like END).
     Token(TokenType type, char value = '\0') : type(type), value(value) {}
+};
+
+/*
+ The Lexer class reads the regular expression pattern and produces tokens corresponding to the different components of the pattern (literals, dot, asterisk, end of input). The nextToken method handles the logic of token generation based on the current character and updates the index accordingly.
+*/
+class Lexer
+{
+public:
+    Lexer(const std::string &pattern) : pattern(pattern), index(0) {}
+
+    Token nextToken()
+    {
+        // Reach to the end of pattern string
+        if (index >= pattern.length())
+        {
+            return Token(TokenType::END);
+        }
+
+        // Get current character
+        char current = pattern[index++];
+
+        switch (current)
+        {
+        case '*':
+            return Token(TokenType::STAR);
+        case '.':
+            return Token(TokenType::DOT);
+        default:
+            // alphabetic literal token
+            if (std::isalpha(current))
+            {
+                return Token(TokenType::LITERAL);
+            }
+            else
+            {
+                throw std::invalid_argument("Invalid pattern string");
+            }
+        }
+    }
+
+private:
+    std::string pattern; // Pattern string
+    size_t index;        // Index for pattern string
 };
